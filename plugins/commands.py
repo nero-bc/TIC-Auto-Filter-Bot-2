791395c012bac9996ae5998b113bcbf5ac8dd042
gbrohit <119616499+gbrohit@users.noreply.github.com>
2023-08-18 19:02:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,7 +87,7 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [
+        buttons = [[
                     InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('〄 Hᴇʟᴘ 〄', callback_data='help'),
