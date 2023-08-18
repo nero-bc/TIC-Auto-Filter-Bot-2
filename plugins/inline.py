004b05c077d326dc033b6576aa19faa834a61d59
gbrohit <119616499+gbrohit@users.noreply.github.com>
2023-08-18 19:45:19 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -22,7 +22,7 @@ async def inline_users(query: InlineQuery):
 
 @Client.on_inline_query()
 async def answer(bot, query):
-    """Show search results for given inline query"""
+    """sʜᴏᴡ sᴇᴀʀᴄʜ ʀᴇsᴜʟᴛs ғᴏʀ ɢɪᴠᴇɴ ɪɴʟɪɴᴇ ǫᴜᴇʀʏ"""
     chat_id = await active_connection(str(query.from_user.id))
     
     if not await inline_users(query):
@@ -35,7 +35,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴛʜᴇ ʙᴏᴛ',
                            switch_pm_parameter="subscribe")
         return
 
@@ -107,11 +107,10 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('sᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
-
 
 
 
